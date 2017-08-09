9b83b79c1d179f3481ddbc4e275e0d75b9bb8e0e
Alana <alana.turangan@gmail.com>
2017-08-08 19:12:23 -0700

    Search Bar Design

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import SearchBar from './components/SearchBar.jsx';
+import styles from './styles.js'; 
+
+injectTapEventPlugin();
 
 class App extends React.Component {
   constructor(props) {
@@ -9,27 +16,72 @@ class App extends React.Component {
     this.state = { 
       items: []
     }
+    this.search = this.search.bind(this); 
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: '/items', 
-      success: (data) => {
-        this.setState({
-          items: data
-        })
-      },
-      error: (err) => {
-        console.log('err', err);
-      }
+  search(input) {
+    this.setState({
+      isLoading: true,
     });
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+      });
+    }, 700);
+    console.log('search: ', input);
+    // axios.get(`/search?query=${input}`)
+    // .then((response) => {
+    //   console.log('RES DATA API IS', response.data);
+    //   this.setState({
+    //     data: response.data,
+    //   }, () => this.setState({
+    //     lat: response.data.lat,
+    //     lng: response.data.lng,
+    //   }));
+    // })
+    // .then(
+    //   this.setState({
+    //     favView: false,
+    //     mainView: true,
+    //   }, this.setState({ mapView: true })),
+    // )
+    // // .then(this.setState({ isLoading: false }))
+    // .catch((error) => {
+    //   if (error) {
+    //     this.setState({
+    //       isLoading: false,
+    //     });
+    //   }
+    //   console.warn(error);
+    // });
+  }
+
+
+  componentDidMount() {
+    // $.ajax({
+    //   url: '/items', 
+    //   success: (data) => {
+    //     this.setState({
+    //       items: data
+    //     })
+    //   },
+    //   error: (err) => {
+    //     console.log('err', err);
+    //   }
+    // });
   }
 
   render () {
-    return (<div>
-      <h1>Url Summary</h1>
-      <List items={this.state.items}/>
-    </div>)
+    return (
+      <MuiThemeProvider>
+        <div>
+          <SearchBar
+            onSearch={ this.search }
+            style={ styles.main }
+          />
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
--- server/app.js
+++ server/app.js
@@ -19,8 +19,4 @@ app.get('/items', function (req, res) {
   });
 });
 
-// app.listen(3000, function() {
-//   console.log('listening on port 3000!');
-// });
-
 module.exports = app; 
